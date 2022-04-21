bd34f88485ce7c619c1cf65c925df17dc5b86656
OlgaMMMM <90023764+OlgaMMMM@users.noreply.github.com>
2022-04-21 11:11:14 +0300

    Update Header1.h

--- Header1.h
+++ Header1.h
@@ -3,32 +3,37 @@
 //#include "Tcountries.cpp"
 using namespace std;
 #pragma once
-
-class answer {
+class answer 
+{
 public:
-    int answer1(); 
+  int answer1(); 
 };
-
-class Tcountries {
+class Tcountries 
+{
 private:
-    int population, square, count_of_city;
+  int population, square, count_of_city;
 public:
-    void set();
-    void get();
+  void set();
+  void get();
 };
-class Number_of_continent {
+class Number_of_continent 
+{
 public:
-    virtual void Number_of_continent1();
+  virtual void Number_of_continent1();
 };
-class Eurasia: public Number_of_continent {
-    void Number_of_continent1() override;
+class Eurasia: public Number_of_continent 
+{
+  void Number_of_continent1() override;
 };
-class Africa : public Number_of_continent {
-    void Number_of_continent1() override;
+class Africa : public Number_of_continent 
+{
+  void Number_of_continent1() override;
 };
-class Australia : public Number_of_continent {
-    void Number_of_continent1() override;
+class Australia : public Number_of_continent 
+{
+  void Number_of_continent1() override;
 };
-class America : public Number_of_continent {
-    void Number_of_continent1() override;
+class America : public Number_of_continent 
+{
+  void Number_of_continent1() override;
 };
