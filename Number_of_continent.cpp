1c37dae8b18a741485d0a10b10b0826335c1c46f
OlgaMMMM <90023764+OlgaMMMM@users.noreply.github.com>
2022-04-21 11:11:35 +0300

    Update Number_of_continent.cpp

--- Number_of_continent.cpp
+++ Number_of_continent.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 #include "Header1.h"
 using namespace std;
-virtual void Number_of_continent::Number_of_continent1() {
+virtual void Number_of_continent::Number_of_continent1() 
+{
   cout << "Choose a number(1-4):" << endl;
   cout << "1.Eurasia" << endl;
   cout << "2.Africa" << endl;
